cbac1ec48ae683271ea496526e641df1aec26212
Scott Young <sayoung@praxiseng.com>
2022-09-08 16:10:54 -0400

    Load ELF section and segment headers to annotate matches with the section offset

--- src/hasher.py
+++ src/hasher.py
@@ -341,7 +341,7 @@ def pretty_fileset(fileset, db = None, overlapping = None):
         #{" ".join(ofids[:30])}  '
 
     if hdt == HashDes.SUMMARY:
-        hash, nmatch, nfiles = fileset
+        hashcode, nmatch, nfiles = fileset
         return f'{nmatch} matches on {nfiles} files'
     return f'??? {fileset}'
 
@@ -1039,6 +1039,8 @@ class FileDB:
 
         self.enum_file_list()
 
+        self.file_offset_thunks = []
+
         # self.db_file = SimpleSectorHashList(self.get_hashes_path())
 
     def get_hashes_path(self):
@@ -1049,6 +1051,37 @@ class FileDB:
 
     def open_db(self):
         return SimpleSectorHashList(self.get_hashes_path())
+
+
+    def add_file_offset_thunk(self, thunk):
+        self.file_offset_thunks.append(thunk)
+
+    def find_thunks(self, address, only_nearest=True):
+        for thunk in self.file_offset_thunks:
+            thunk_results = []
+            for a, b, meta in thunk.all_in_range(address):
+                trans = try_translate(address, a, b)
+                thunk_off = address - a[0]
+
+                thunk_results.append((meta['name'], trans, thunk_off, meta))
+
+            if only_nearest:
+                if not thunk_results:
+                    continue
+                thunk_results = sorted(thunk_results, key = lambda result : result[2])
+                yield thunk_results[0]
+            else:
+                for result in thunk_results:
+                    yield result
+
+    def thunk_txt(self, address):
+        txt = []
+        for name, new_addr, thunk_off, meta in self.find_thunks(address):
+            if name:
+                txt.append(f'{name}+{thunk_off:x}')
+            else:
+                txt.append('??')
+        return ' '.join(txt)
 
     def enum_file_list(self):
         # self.all_files = []
@@ -1268,7 +1301,7 @@ class MatchRun:
         if HashDes.MATCH_LIST == hdType(b):
             self.fds = set(fid for fid, offset in b[1])
 
-        print(f'mr {self.offset:x}-{self.end:x} {self.l:x}')
+        #print(f'mr {self.offset:x}-{self.end:x} {self.l:x}')
 
     def can_merge(self, other):
         if other.offset != self.end:
@@ -1507,6 +1540,111 @@ def group_matchsets(matchsets, db):
         #{"    ".join(close_txt)}
 
 
+
+def try_translate(address, a_range, b_range):
+    a_lo, a_len = a_range
+    if a_lo <= address < a_lo + a_len:
+        b_lo, b_len = b_range
+        a_off = address - a_lo
+
+        if a_off < b_len:
+            return b_lo + a_off
+
+    return None
+
+
+
+
+class Thunk:
+    def __init__(self, a_name, b_name):
+        self.a_name = a_name
+        self.b_name = b_name
+
+        self.map = []
+
+    def add(self, a_range, b_range, meta):
+        self.map.append((a_range, b_range, meta))
+
+
+    def all_in_range(self, address):
+        for a_range, b_range, meta in self.map:
+            a_lo, a_len = a_range
+            if a_lo <= address < a_lo + a_len:
+                yield a_range, b_range, meta
+
+    def all_in_range_reverse(self, address):
+        for a_range, b_range, meta in self.map:
+            b_lo, b_len = b_range
+            if b_lo <= address < b_lo + b_len:
+                yield a_range, b_range, meta
+
+    def translate(self, address):
+        for a_range, b_range, meta in self.all_in_range(address):
+            new_off = try_translate(address, a_range, b_range)
+            if new_off != None:
+                return new_off, meta
+        return None, None
+
+    def inverse_translate(self, address):
+        for a_range, b_range, meta in self.all_in_range_reverse(address):
+            new_off = try_translate(address, b_range, a_range)
+            if new_off != None:
+                return new_off, meta
+        return None, None
+
+
+from elftools.elf.elffile import ELFFile
+class ELFThunks:
+    def __init__(self, path):
+        with open(path, 'rb') as f:
+            elffile = ELFFile(f)
+
+            self.section_thunk = Thunk('File Offset', 'Virtual Address')
+            self.segment_thunk = Thunk('File Offset', 'Virtual Address')
+
+            for sect in elffile.iter_sections():
+                flags = sect.header['sh_flags']
+                flag_map = [(2, 'A'), (1, 'W'), (4, 'X')]
+                flag_txt = ''.join(txt if (flags & mask) else ' ' for mask, txt in flag_map)
+
+                sh_type = sect.header['sh_type']
+                sh_addr = sect.header['sh_addr']  # first virtual address
+                sh_offset = sect.header['sh_offset']  # file offset
+                sh_size = sect.header['sh_size']
+
+                allocated = (flags & 2) != 0
+                has_file_bytes = 'SHT_NOBITS' != sh_type
+
+                mem_size = sh_size if allocated else 0
+                file_size = sh_size if has_file_bytes else 0
+
+                meta = dict(name = sect.name or sh_type, struct = dict(sect.header))
+                self.segment_thunk.add((sh_offset,file_size), (sh_addr,mem_size), meta)
+
+
+                print(
+                    f'Section {sect.name:20} {flag_txt} {sh_offset:6x}+{file_size:<6x}  {sh_addr:6x}+{mem_size:<6x} {sect.header}')
+
+            for seg in elffile.iter_segments():
+                p_type = seg.header['p_type']
+                p_offset = seg.header['p_offset']
+                p_vaddr = seg.header['p_vaddr']
+                p_paddr = seg.header['p_paddr']
+                p_filesz = seg.header['p_filesz']
+                p_memsz = seg.header['p_memsz']
+
+                flags = seg.header['p_flags']
+
+                flagmap = [(4, 'R'), (2, 'W'), (1, 'X')]
+                flagtxt = ''.join(txt if (flags & mask) else ' ' for mask, txt in flagmap)
+
+                meta = dict(name = p_type, struct = dict(seg.header))
+                self.section_thunk.add((p_offset, p_filesz), (p_vaddr, p_memsz), meta)
+
+
+                print(f'Segment {p_type:20} {flagtxt} {p_offset:6x}+{p_filesz:<6x} {p_vaddr:6x}+{p_memsz:<6x} {dict(seg.header)}')
+
+
 def main():
     parser = argparse.ArgumentParser(description='Sector hashing tool')
     parser.add_argument('db', metavar='PATH', help='The database path')
@@ -1522,14 +1660,16 @@ def main():
     global ZEROIZE_X86_PC_REL
     ZEROIZE_X86_PC_REL = args.zeroize
 
-
     db = FileDB(args.db, args.blocksize)
 
-    if (args.ingest):
+    if args.ingest:
         db.ingest(args.ingest)
 
-
     if args.search:
+        et = ELFThunks(args.search)
+        db.add_file_offset_thunk(et.section_thunk)
+        db.add_file_offset_thunk(et.segment_thunk)
+
 
         search_results = list(db.rollingSearch(args.search, step=args.step))
         search_results = sorted(search_results, key=lambda a: a[0][1])
@@ -1584,8 +1724,10 @@ def main():
 
 
             present_txt = ' '.join(sorted(fid_to_names(present, db)))
+
+            tt = db.thunk_txt(off)
             #present_txt = bitmask_fids(present)
-            print(f"  {off:5x}+{runlen:<5x} {sim:5.3f} {count:4} {len(present):4}   {present_txt[:300]}")
+            print(f"  {off:5x}+{runlen:<5x} {tt:32} {sim:5.3f} {count:4} {len(present):4}   {present_txt[:300]}")
             #present_txt = ' '.join(sorted(fid_to_names(present, db)))
             #print(f"  {off:5x}+{runlen:<5x} {count:4} {' '.join(present_txt[:30])}"
 
