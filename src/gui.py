1dc1000435eb0ddb2b476d9c985dc1b6bce2985c
resrever <sayoung@praxiseng.com>
2023-04-18 20:44:26 -0400

    Add support for dragging the file match view left and right.

--- src/gui.py
+++ src/gui.py
@@ -31,19 +31,12 @@ class FileView:
 
         #self.file_set_at_offset = None
 
-
         self.cumulative_counts = None
         self.fid_to_name = None
 
-        self.init_graph()
 
-        self.offset_count_intervals: IntervalTree | None = None
-
-        self.file_x_start = 0
-        self.file_x_end = len(self.contents)
-
-    def init_graph(self):
-        self.canvas_width = 1000
+        screenWidth, screenHeight = sg.Window.get_screen_size()
+        self.canvas_width = screenWidth * 80 // 100
         self.canvas_coord_width = 10000
 
         canvas_size = (self.canvas_width, 300)
@@ -59,11 +52,23 @@ class FileView:
                       key='-BYTES-')]
         ]
 
-        self.window = sg.Window(f'Sector Hash {self.path}', layout, finalize=True, resizable=True, background_color='light grey')
+        self.window = sg.Window(f'Sector Hash {self.path}',
+                                layout,
+                                location=(screenWidth*1//10, screenHeight*1/10),
+                                finalize=True,
+                                resizable=True,
+                                background_color='light grey')
         self.window.refresh()
         self.graph = self.window['graph']
         self.text_data_display = self.window['-BYTES-']
 
+        self.offset_count_intervals: IntervalTree | None = None
+
+        self.file_x_start = 0
+        self.file_x_end = len(self.contents)
+
+        self.drag_start_x = None
+        self.drag_start_x_file = None
 
     def canvas_to_file_offset(self, x: int) -> int:
         file_view_width = self.file_x_end - self.file_x_start
@@ -141,7 +146,7 @@ class FileView:
         self.draw_file_sets()
 
     def adjust_sizes(self):
-        print(f'Window size {self.window.size}')
+        print(f'Window size {self.window.size}  {self.window.get_screen_size()}')
 
         self.canvas_width = self.window.size[0]-30
         self.graph.set_size((self.canvas_width, 300))
@@ -231,6 +236,32 @@ class FileView:
                 continue
             self.offset_count_intervals[oc.offset:oc.offset+oc.length] = oc
 
+    def handle_mouse_wheel(self):
+        # print(f"Mouse {self.graph.user_bind_event.delta}")
+        mouse_steps = int(self.graph.user_bind_event.delta / 120)
+        file_range = self.file_x_end - self.file_x_start
+        if mouse_steps < 0:
+            # Zoom out
+            self.file_x_start -= file_range // 5
+            self.file_x_end += file_range // 5
+
+        else:
+            # Zoom in
+            file_hover_x = self.canvas_to_file_offset(self.hover_x)
+            left = file_hover_x - self.file_x_start
+            right = self.file_x_end - file_hover_x
+            self.file_x_start += left // 5
+            self.file_x_end -= right // 5
+
+        # print(f'X range {self.file_x_start}  {self.file_x_end}')
+        overshoot = len(self.contents) // 20
+
+        if self.file_x_start < -overshoot:
+            self.file_x_start = -overshoot
+        if self.file_x_end > len(self.contents) + overshoot:
+            self.file_x_end = len(self.contents) + overshoot
+
+        self.redraw_graph()
 
     def event_loop(self):
         self.adjust_sizes()
@@ -239,43 +270,33 @@ class FileView:
         self.graph.bind('<Motion>', '+MOTION+')
         self.graph.bind('<MouseWheel>', '+MOUSEWHEEL+')
 
+        self.graph.bind('<Button-1>', '+CLICK+')
+        self.graph.bind('<B1-Motion>', '+DRAG+')
+
         while True:
             event, values = self.window.read()
             if event in ('Quit', None):  # always give ths user a way out
                 break
 
-            #print(f'Event={event}, values = {values}')
+            # print(f'Event={event}, values = {values}')
+
+            self.hover_x, self.hover_y = values['graph']
+
             if 'RESIZE' in event:
                 self.adjust_sizes()
             if 'MOUSEWHEEL' in event:
-                # print(f"Mouse {self.graph.user_bind_event.delta}")
-                mouse_steps = int(self.graph.user_bind_event.delta/120)
-                file_range = self.file_x_end - self.file_x_start
-                if mouse_steps < 0:
-                    # Zoom out
-                    self.file_x_start -= file_range // 5
-                    self.file_x_end += file_range // 5
+                self.handle_mouse_wheel()
+            if 'CLICK' in event:
+                self.drag_start_x = self.hover_x
+                self.drag_start_x_file = self.canvas_to_file_offset(self.drag_start_x)
+            if 'DRAG' in event:
+                file_x = self.canvas_to_file_offset(self.hover_x)
+                delta_x = self.drag_start_x_file - file_x
 
-                else:
-                    # Zoom in
-                    file_hover_x = self.canvas_to_file_offset(self.hover_x)
-                    left = file_hover_x - self.file_x_start
-                    right = self.file_x_end - file_hover_x
-                    self.file_x_start += left // 5
-                    self.file_x_end -= right // 5
-
-                # print(f'X range {self.file_x_start}  {self.file_x_end}')
-                overshoot = len(self.contents) // 10
-
-                if self.file_x_start < -overshoot:
-                    self.file_x_start = -overshoot
-                if self.file_x_end > len(self.contents) + overshoot:
-                    self.file_x_end = len(self.contents) + overshoot
+                self.file_x_start += delta_x
+                self.file_x_end += delta_x
 
                 self.redraw_graph()
-
-
-            self.hover_x, self.hover_y = values['graph']
 
             self.update_text()
             self.update_hover_line()
